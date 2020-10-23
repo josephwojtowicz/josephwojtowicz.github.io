7ce30dd356f666c08776f2c2eb0e01593b23dba0
josephwojtowicz <69682505+josephwojtowicz@users.noreply.github.com>
2020-10-23 18:30:20 -0400

    Update script.js

--- joe_wojtowicz_homework_6/script.js
+++ joe_wojtowicz_homework_6/script.js
@@ -27,7 +27,7 @@ getItems();
 function getData() {
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=642f9e3429c58101eb516d1634bdaa4b"
     mainCard.empty();
-
+    $("#weeklyForecast").empty();
     $.ajax({
         url: queryURL,
         method: "GET"
